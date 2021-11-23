45d27f0abd522e7c19ae3fb11e67d25add83259e
Hendri Susanto <hendri.susanto@softorb.co.id>
2021-11-23 16:56:03 +0700

    First Commit

---
+++ Barcode/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.barTB = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.decodeBtn = new System.Windows.Forms.Button();
+            this.decodeTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(457, 30);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(75, 23);
+            this.generateBtn.TabIndex = 0;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // barTB
+            // 
+            this.barTB.Location = new System.Drawing.Point(29, 30);
+            this.barTB.Name = "barTB";
+            this.barTB.Size = new System.Drawing.Size(397, 20);
+            this.barTB.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(29, 122);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(749, 316);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(571, 27);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // decodeBtn
+            // 
+            this.decodeBtn.Location = new System.Drawing.Point(457, 74);
+            this.decodeBtn.Name = "decodeBtn";
+            this.decodeBtn.Size = new System.Drawing.Size(75, 23);
+            this.decodeBtn.TabIndex = 4;
+            this.decodeBtn.Text = "Decode";
+            this.decodeBtn.UseVisualStyleBackColor = true;
+            this.decodeBtn.Click += new System.EventHandler(this.decodeBtn_Click);
+            // 
+            // decodeTB
+            // 
+            this.decodeTB.Location = new System.Drawing.Point(29, 76);
+            this.decodeTB.Name = "decodeTB";
+            this.decodeTB.Size = new System.Drawing.Size(397, 20);
+            this.decodeTB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.decodeTB);
+            this.Controls.Add(this.decodeBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.barTB);
+            this.Controls.Add(this.generateBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.TextBox barTB;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button decodeBtn;
+        private System.Windows.Forms.TextBox decodeTB;
+    }
+}
+
---
+++ Barcode/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using QRCoder;
+using ZXing.QrCode;
+using ZXing;
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+
+            //QRCodeGenerator qr = new QRCodeGenerator();
+            //QRCodeData data = qr.CreateQrCode(barTB.Text, QRCodeGenerator.ECCLevel.Q);
+            //data.SaveRawData("QRCode",QRCodeData.Compression.Uncompressed);
+            //QRCode code = new QRCode(data);
+            //pictureBox.Image = code.GetGraphic(5);
+
+            var options = new QrCodeEncodingOptions
+            {
+                Height = pictureBox.Height,
+                Width = pictureBox.Width
+            };
+
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+            var text = barTB.Text;
+
+            if(text == "")
+            {
+                MessageBox.Show("Please Insert The Text!!!");
+                return;
+            }
+
+            var result = writer.Write(text);
+            pictureBox.Image = result;
+
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            string location = @"C:\Users\Hendri\source\repos\Barcode\QRImages";
+            var dialog = new SaveFileDialog();
+
+            dialog.InitialDirectory = location;
+            dialog.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image.Save(dialog.FileName);
+            }
+        }
+
+        private void decodeBtn_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var reader = new BarcodeReader();
+                var imgFile = Image.FromFile(dialog.FileName) as Bitmap;
+                pictureBox.Image = imgFile;
+                var result = reader.Decode(imgFile);
+                decodeTB.Text = result.Text;
+            }
+        }
+    }
+}
